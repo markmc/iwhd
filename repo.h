8be7a4155657dc35bc2e51c70d1ef0d2aa809827
Jim Meyering <meyering@redhat.com>
2010-08-30 17:00:55 +0200

    maint: define macros used to teach compiler about semantics
    * repo.h (__attribute__, ATTRIBUTE_UNUSED): Define.

--- repo.h
+++ repo.h
@@ -37,3 +37,12 @@ GLOBAL(char *,          me,             "here")
 #define P2I(x)	((int)(long)(x))
 #define DPRINTF	verbose && printf
 
+#ifndef __attribute__
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 8)
+#  define __attribute__(x) /* empty */
+# endif
+#endif
+
+#ifndef ATTRIBUTE_UNUSED
+# define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+#endif
