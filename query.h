c50990b6720b80e31ae13461a2959502d5cb2650
Jeff Darcy <jdarcy@redhat.com>
2010-09-22 11:58:45 -0400

    More changes suggested by Jim, made by me - const pointers, unsigned ints, and so on.

--- query.h
+++ query.h
@@ -36,9 +36,9 @@ typedef struct {
 
 
 value_t	*parse		(const char *text);
-int	 eval		(value_t *expr, getter_t *oget, getter_t *sget);
-void	 free_value	(value_t *);
-void	 print_value	(value_t *);
+int	 eval		(const value_t *expr, getter_t *oget, getter_t *sget);
+void	 free_value	(const value_t *);
+void	 print_value	(const value_t *);
 
 #if defined(__CPLUSPLUS__) || defined(__cplusplus)
 }
